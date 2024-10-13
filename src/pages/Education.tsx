bdc39f88cfc0b368d2d6d8fc18eeaa6acae1abe6
shaik-baleeghuddin-kashif <hello@sbkashif.com>
2024-10-13 15:07:11 +0530

    Adding Education temp page

--- src/pages/Education.tsx
+++ src/pages/Education.tsx
@@ -1,33 +1,35 @@
 
 const Education = () => {
-  // const education = [
-  //   {
-  //     id: 1,
-  //     degree: "Bachelor of Technology",
-  //     department: "Electronics and Communication Engineering",
-  //     logo: "/bvrit-logo.png",
-  //     institute: "B. V. Raju Institute of  Technology",
-  //     start:  "2019",
-  //     end:  "2023",
-  //     gpa: "7.3/10",
-  //     bio: "",
-  //     points: []
-  //   }
-  // ]
   return (
     <div
-    className="flex h-full w-full p-2 md:p-12 bg-white bg-opacity-10 backdrop-blur-lg shadow-lg border-white/30 border-2 rounded-lg"
+    className="flex flex-col md:flex-row w-full h-full bg-white bg-opacity-10 backdrop-blur-lg shadow-lg border-white/30 border-2 rounded-lg p-4 md:p-8 text-sm md:text-base"
     >
-      <div
-      className="flex flex-col md:flex-row p-2 w-full h-fit bg-red-500"
-      >
-        <div
-        className="w-full md:w-[10%]"
-        >
-          A
+      <div className="flex flex-col md:flex-row w-full h-fit md:p-2 gap-3">
+        {/* <!-- Left Section --> */}
+        <div className="flex gap-x-3 md:flex-col items-center w-full md:w-[30%] p-3 rounded-lg space-y-3">
+          {/* <!-- Logo Container --> */}
+          <div className="flex  justify-center items-center w-12 h-12 md:w-32 md:h-32 bg-white bg-opacity-10 backdrop-blur-lg border border-white/30 rounded-lg shadow-md">
+            <div className="w-10 h-10 md:w-24 md:h-24 bg-[url('/bvrit-logo.png')] bg-cover bg-center bg-no-repeat rounded-lg"></div>
+          </div>
+          {/* <!-- Institute Information --> */}
+          <div className="text-center space-y-1 text-sm">
+            <div className="font-extrabold text-gray-900 text-[12px] md:text-lg">B. V. Raju Institute of Technology</div>
+            <div className="text-gray-800 font-bold text-[9px] md:text-[16px]">Narsapur, Medak, Telangana</div>
+            <div className="text-gray-800 font-bold text-[9px] md:text-[16px]">(2019 - 2023)</div>
+          </div>
         </div>
-        <div>
-          B
+        
+        {/* <!-- Right Section --> */}
+        <div className="flex flex-col w-full md:w-[70%] p-3 rounded-lg space-y-2">
+          <div className="flex flex-col md:flex-row justify-between text-gray-900 font-semibold text-sm md:text-lg">
+            <div>Electronics & Communication Engineering</div>
+            <div>GPA: (7.3/10)</div>
+          </div>
+          {/* <!-- Placeholder for Course Content --> */}
+          <p className="text-xs text-gray-800 leading-tight">
+            {/* Replace this with your course content */}
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vehicula est at sapien euismod, eget viverra lacus tristique.
+          </p>
         </div>
       </div>
     </div>
